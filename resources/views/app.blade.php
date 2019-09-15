af5d69896623c31224c14e5df35a45d721c491dd
Léu Almeida <leo@webid.net.br>
2019-09-14 23:44:50 -0300

    Views update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,1 +1,7 @@
 <h1>APP</h1>
+
+<div>
+    <a href="{{route('app.user')}}">User</a> <br/>
+    <a href="{{route('app.profile')}}">Profile</a> <br/>
+    <a href="{{route('app')}}">App</a> <br/>
+</div>
