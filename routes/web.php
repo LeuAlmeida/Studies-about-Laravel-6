93c17b20268dfbed538d99d36c642df081dca7c2
Léu Almeida <leo@webid.net.br>
2019-09-14 22:38:25 -0300

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,41 @@ Route::get('/', function () {
 });
 
 
-/////////////////////////////////////
-// Criação de rotas com parâmetros //
-/////////////////////////////////////
+// Parâmetro das Rotas
+Route::get('/ola/{nome}/{sobrenome}', function($nome, $sobrenome) {
+    echo "Olá! Seja bem vindo, $nome $sobrenome!";
+});
 
-// Route::get('ola/{nome}', function($nome) {
-//     echo "Olá! Seja bem vindo, " . $nome . "!";
-// });
+
+// Parâmetros Opcionais
+Route::get('/seunome/{nome?}', function($nome=null) {
+    if(isset($nome))
+        echo "Olá! Seja bem vindo, $nome!";
+    else
+        echo "Você não digitou nenhum nome.";
+});
+
+
+// Parâmetro com Regras
+Route::get('/rotacomregras/{nome}/{n}', function($nome, $n) {
+    for($i=0;$i<$n;$i++)
+    echo "Olá! Seja bem vindo, $nome!" . "<br/>";
+})->where('nome','[A-Za-z]+')->where('n','[0-9]+');
+
+
+// Agrupamento de Rotas
+Route::prefix('/app')->group(function() {
+    
+    Route::get('/', function() {
+        return view('app');
+    });
+
+    Route::get('/user', function() {
+        return view('user');
+    });
+
+    Route::get('/profile', function() {
+        return view('profile');
+    });
+
+});
