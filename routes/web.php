532af46e5e018dab5fe8da2edca560d3d6e86578
Léu Almeida <leo@webid.net.br>
2019-09-14 23:58:07 -0300

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -53,4 +55,64 @@ Route::prefix('/app')->group(function() {
         return view('profile');
     });
 
+});
+
+
+// Nomeando Rotas
+Route::prefix('/aplicacao')->group(function() {
+    
+    Route::get('/', function() {
+        return view('app');
+    })->name('app');
+
+    Route::get('/user', function() {
+        return view('user');
+    })->name('app.user');
+
+    Route::get('/profile', function() {
+        return view('profile');
+    })->name('app.profile');
+
+});
+
+Route::get('/produtos', function() {
+    echo "<h1>Produtos</h1>";
+    echo "<ol>";
+    echo "<li>Notebook </li>";
+    echo "<li>Impressora </li>";
+    echo "<li>Mouse </li>";
+    echo "</ol>";
+})->name('meusprodutos');
+
+// Redirecionando Requisições
+Route::redirect('meusprodutos1', 'produtos', 301);
+
+Route::get('todosprodutos2', function() {
+    return redirect()->route('meusprodutos');
+});
+
+////////////////////////////////////////////////////
+
+Route::post('/requisicoes', function(Request $request) {
+    return 'Hello POST';
+});
+
+Route::delete('/requisicoes', function(Request $request) {
+    return 'Hello DELETE';
+});
+
+Route::put('/requisicoes', function(Request $request) {
+    return 'Hello PUT';
+});
+
+Route::patch('/requisicoes', function(Request $request) {
+    return 'Hello PATCH';
+});
+
+Route::options('/requisicoes', function(Request $request) {
+    return 'Hello OPTIONS';
+});
+
+Route::get('/requisicoes', function(Request $request) {
+    return 'Hello GET';
 });
